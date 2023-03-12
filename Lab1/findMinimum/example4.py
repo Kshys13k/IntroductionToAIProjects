a42315821d4f948f2b2bc2e7a823c0864d7940ff
kshys13k <kkterlecki@gmail.com>
2023-03-12 12:43:56 +0100

    reinstalled cec2017 functions package, implemented example from github (example4.py). cec2017 works

---
+++ Lab1/findMinimum/example4.py
@@ -0,0 +1,17 @@
+import numpy as np
+# Using only f5:
+from cec2017.functions import f5
+samples = 3
+dimension = 50
+x = np.random.uniform(-100, 100, size=(samples, dimension))
+val = f5(x)
+for i in range(samples):
+    print(f"f5(x_{i}) = {val[i]:.6f}")
+
+# Using all functions:
+from cec2017.functions import all_functions
+for f in all_functions:
+    x = np.random.uniform(-100, 100, size=(samples, dimension))
+    val = f(x)
+    for i in range(samples):
+        print(f"{f.__name__}(x_{i}) = {val[i]:.6f}")
